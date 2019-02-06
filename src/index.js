7cc92ba4d91579d595bcfccccd1986976925d0ed
madhusudhand <madhusudhan.dollu@gmail.com>
2019-02-06 16:30:59 +0530

    update index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const blob = require('./lib/blob');
 const tree = require('./lib/tree');
 const commit = require('./lib/commit');
 const branch = require('./lib/branch');
+const diff = require('./lib/diff');
 const gitConfig = require('./lib/git-config');
 
 module.exports = {
@@ -12,4 +13,5 @@ module.exports = {
   tree,
   commit,
   branch,
+  diff,
 };
